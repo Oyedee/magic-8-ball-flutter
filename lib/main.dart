a83d5a82e01c5398633fe8567105b7080a74f5de
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2022-01-30 17:22:21 +0100

    Magic ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('Magic Ball'),
+            backgroundColor: Colors.blueGrey[800],
+          ),
+          body: MagicBallPage(),
+        ),
       ),
     );
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({Key? key}) : super(key: key);
+
+  @override
+  _MagicBallPageState createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int magicBallNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            generateMagicBall();
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Image.asset('images/ball$magicBallNumber.png'),
+        ),
+      ),
+    );
+  }
+
+  void generateMagicBall() {
+    magicBallNumber = Random().nextInt(5) + 1;
+  }
+}
